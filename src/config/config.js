9b1ab4805424dfa86154db7b72cb1da003bfff13
Joel Carlos <joel.carlos@merkle.com>
2025-10-30 10:06:13 +0800

    rollbak

--- src/config/config.js
+++ src/config/config.js
@@ -32,7 +32,11 @@ module.exports = {
     },
   },
   production: {
-    use_env_variable: "DATABASE_URL",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "postgres",
     dialectOptions: {
       ssl: {
