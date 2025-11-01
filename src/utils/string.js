f3dc86e32e1b487771a96dbc623f708688b685e7
Joel Carlos <joel.carlos@merkle.com>
2025-11-01 13:44:55 +0800

    fixed typo

--- src/utils/string.js
+++ src/utils/string.js
@@ -8,7 +8,7 @@ function shortenNameTo(str, length = 3) {
   // const words = name.replace(/[^a-zA-Z0-9 ]/g, "").split(" ");
   const words = name
     .replace(/[^a-zA-Z0-9 \u00BC-\u00BE\u2150-\u215E]/g, "")
-    .split(/\s+/);
+    .split(" ");
 
   // Take first 2-3 letters of each word (max 3 words)
   const shortened = words
