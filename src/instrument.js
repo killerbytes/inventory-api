5e0af0672b5295ff32d167f5eb475d6e18c85e67
Joel Carlos <joel.carlos@merkle.com>
2026-03-02 14:12:35 +0800

    Added sentry

---
+++ src/instrument.js
@@ -0,0 +1,9 @@
+// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
+const Sentry = require("@sentry/node");
+
+Sentry.init({
+  dsn: "https://0cfaab89ff758bf255f5bbece79afb96@o4510944413483008.ingest.us.sentry.io/4510961091215360",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+});
