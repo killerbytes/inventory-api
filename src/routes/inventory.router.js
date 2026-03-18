a1661fd38c4e8656f56e96e264a966cc36b1989f
Joel Carlos <joel.carlos@merkle.com>
2026-03-18 16:52:04 +0800

    rename urls

--- src/routes/inventory.router.js
+++ src/routes/inventory.router.js
@@ -3,10 +3,10 @@ const inventoryController = require("../controllers/inventory.controller");
 const router = express.Router();
 
 router.post("/movements", inventoryController.getMovements);
-router.post("/breakPacks", inventoryController.getBreakPacks);
-router.post("/stockAdjustments", inventoryController.getStockAdjustments);
-router.post("/priceHistory", inventoryController.getPriceHistory);
-router.get("/reorderLevels", inventoryController.getReorderLevels);
+router.post("/break-packs", inventoryController.getBreakPacks);
+router.post("/stock-adjustments", inventoryController.getStockAdjustments);
+router.post("/price-history", inventoryController.getPriceHistory);
+router.get("/reorder-levels", inventoryController.getReorderLevels);
 router.get("/:id/return-transaction", inventoryController.getReturnTransaction);
 router.get("/:id/return-items", inventoryController.getReturnItems);
 
